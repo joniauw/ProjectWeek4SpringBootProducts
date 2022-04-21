9af4040bd6946dc778e3286b739ced676e733924
joniauw <78723248+joniauw@users.noreply.github.com>
2022-04-21 16:59:52 +0200

    added auth code from other repo

--- src/main/java/com/example/partyinaboxweb/ProductController.java
+++ src/main/java/com/example/partyinaboxweb/ProductController.java
@@ -3,10 +3,11 @@ package com.example.partyinaboxweb;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @CrossOrigin
 @RestController
@@ -19,4 +20,35 @@ public class ProductController {
     public List<Product> getAllProducts() {
         return (List<Product>) repo.findAll();
     }
+
+    @GetMapping("/check")
+    public boolean greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+                            @AuthenticationPrincipal Jwt accessToken) {
+        System.out.println("In GET Request");
+        String scope = accessToken.getClaims().get("scope").toString();
+        Boolean partnerRole = scope.contains("partner");
+        System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString());
+        System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString().contains("partner"));
+        if (partnerRole) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/products")
+    public String addProduct(@RequestBody Product product, @AuthenticationPrincipal Jwt accessToken) {
+        System.out.println("In POST Request");
+        String scope = accessToken.getClaims().get("scope").toString();
+        Boolean partnerRole = scope.contains("partner");
+
+        if (partnerRole) {
+            System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString());
+            System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString().contains("partner"));
+            return "Product added";
+        } else {
+            return "Not Authorized to add product";
+        }
+    }
+
 }
